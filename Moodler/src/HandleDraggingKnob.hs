7993409c0f58c8d0086d33d35586a1b5e117db7d
Dan Piponi <dpiponi@gmail.com>
2015-08-17 08:38:37 -0700

    More tweaks to event handlers

--- Moodler/src/HandleDraggingKnob.hs
+++ Moodler/src/HandleDraggingKnob.hs
@@ -22,6 +22,39 @@ import Music
 import Numeric
 import Text
 
+handleDraggingKnob :: MoodlerM Zero -> (Event -> MoodlerM Zero) -> UiId -> Float -> Point -> 
+                        MoodlerM Zero
+handleDraggingKnob handleDefault handleDefault' selectedKnob v (x0, y0) = do
+    e <- getEvent
+    handleDraggingKnob' v (x0, y0) e
+
+    where
+
+    handleDraggingKnob' :: Float -> Point -> Event -> MoodlerM Zero
+    handleDraggingKnob' v p0 (EventMotion p) = do
+        let newV = knobMapping v (p-p0)
+        -- Use zoom?
+        elts <- use (serverState . uiElements)
+        let elt = M.lookup selectedKnob elts
+        case elt of
+            Nothing -> handleDraggingKnob handleDefault handleDefault' selectedKnob v p0
+            Just e -> do
+                let lowLimit = _knobMin e
+                let highLimit = _knobMax e
+                let v1 = clampToRange lowLimit highLimit newV
+                gadget .= knobGadget p0 v1
+                void $ W.synthSet selectedKnob v1
+                handleDraggingKnob handleDefault handleDefault' selectedKnob v p0
+
+    handleDraggingKnob' _ _
+        (EventKey (MouseButton LeftButton) Up _ _) = do
+        gadget .= const blank
+        doSelection selectedKnob
+        handleDefault
+
+    -- Was handleDefault'. Why? XXX
+    handleDraggingKnob' _ _ e = handleDefault' e
+
 knobGadget :: (Float, Float) -> Float -> B.Transform -> Picture
 knobGadget (x0, y0) v1 xform = 
              pictureTransformer xform $
@@ -32,35 +65,3 @@ knobGadget (x0, y0) v1 xform =
 
 knobMapping :: Float -> Point -> Float
 knobMapping v (dx, dy) = v+0.01*dx*exp (0.01*dy)
-
-handleDraggingKnob :: MoodlerM Zero -> (Event -> MoodlerM Zero) -> UiId -> Float -> Point -> 
-                        MoodlerM Zero
-handleDraggingKnob handleDefault handleDefault' selectedKnob v (x0, y0) = do
-    e <- getEvent
-    handleDraggingKnob' handleDefault handleDefault' selectedKnob v (x0, y0) e
-
-handleDraggingKnob' :: MoodlerM Zero -> (Event -> MoodlerM Zero) ->
-                       UiId -> Float -> Point -> Event -> MoodlerM Zero
-handleDraggingKnob' handleDefault handleDefault' selectedKnob v p0 (EventMotion p) = do
-    let newV = knobMapping v (p-p0)
-    -- Use zoom?
-    elts <- use (serverState . uiElements)
-    let elt = M.lookup selectedKnob elts
-    case elt of
-        Nothing -> handleDraggingKnob handleDefault handleDefault' selectedKnob v p0
-        Just e -> do
-            let lowLimit = _knobMin e
-            let highLimit = _knobMax e
-            let v1 = clampToRange lowLimit highLimit newV
-            gadget .= knobGadget p0 v1
-            void $ W.synthSet selectedKnob v1
-            handleDraggingKnob handleDefault handleDefault' selectedKnob v p0
-
-handleDraggingKnob' handleDefault _ selectedKnob _ _
-    (EventKey (MouseButton LeftButton) Up _ _) = do
-    gadget .= const blank
-    doSelection selectedKnob
-    handleDefault
-
--- Was handleDefault'. Why? XXX
-handleDraggingKnob' _ handleDefault'  _ _ _ e = handleDefault' e
