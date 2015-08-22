834e0190289b729b5f64411dce102b78180eb89b
barnabyrobson <test@test-accounts-macbook-pro.local>
2015-08-22 11:12:53 -0700

    Audio callback sends audio via UDP.
    Using UDP we send the summed audio buffer to a (currently) hard coded
    port. This will allow clients to connect to the audio stream.

--- Moodler/audio.c
+++ Moodler/audio.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include "portaudio.h"
 
 /*
@@ -12,9 +15,16 @@
 #define SAMPLE_RATE 48000
 #define MAX_VOICES 16
 
+#define AUDIO_SERVER_PORT 7356
+#define AUDIO_CLIENT_PORT 7357
+
 const int samplesPerBlock = 256;
 const int samplesPerBuffer = BUFFER_SIZE/sizeof(SAMPLE_TYPE)/NUM_CHANNELS;
 const int blocksPerBuffer = samplesPerBuffer/samplesPerBlock;
+
+int sockfd;
+int socket_ok;
+struct sockaddr_in dest_addr;
 
 /*
  * These are exported to Haskell
@@ -82,7 +92,49 @@ int callback(const void *input,
             sample_buffer[k] += moodler_buffer[i][k];
         }
     }
+
+    if (socket_ok) {
+        if (sendto(sockfd, sample_buffer, BUFFER_SIZE, 0,
+               (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0){
+            perror("sendto failed");
+        }
+    }
     return paContinue;
+}
+
+int setup_audio_server_socket() {
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("cannot create socket");
+        return 0;
+    }
+
+    // bind the socket to any valid IP address and the specific port
+    struct sockaddr_in myaddr;
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    myaddr.sin_port = htons(AUDIO_SERVER_PORT);
+
+    if (bind(sockfd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        perror("bind failed");
+        return 0;
+    }
+
+    // NOTE.
+    // For this implementation we have a hard coded destination
+    // port & address so the address struct can be set up as a global
+    // once during initialization. But we should consider
+    // having this server respond to a 'connection' and
+    // to route the audio to the specific client or clients
+    // This would require a check inside the loop for new clients
+    // which could be complicated.
+    memset((char *)&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    dest_addr.sin_port = htons(AUDIO_CLIENT_PORT);
+
+    return 1;
 }
 
 void play() {
@@ -118,6 +170,11 @@ void play() {
 
     err = Pa_StartStream( stream );
     if( err != paNoError ) goto error;
+
+    socket_ok = setup_audio_server_socket();
+    if (!socket_ok) {
+        fprintf(stderr, "Warning: could not set up audio server socket.\n");
+    }
     return;
 
 error:
